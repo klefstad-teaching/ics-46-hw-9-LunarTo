d7e7fb3b7e029f3b83799890eeca46d229ec034d
LunarTo <iresarej@uci.edu>
2025-03-16 08:14:17 +0000

    test

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -148,10 +148,13 @@ void load_words(set<string> & word_list, const string& file_name){
 
 void print_word_ladder(const vector<string>& ladder){
     if(ladder.empty()){
-        cout << "No word ladder found." << endl;
+        cout << "No word ladder found.";
     }
-    for(int i = 0; i < ladder.size() ; ++i){
-        cout << ladder[i] << " ";
+    else{
+        cout <<"Word ladder found: ";
+        for(int i = 0 ; i < ladder.size() ; ++i){
+            cout << ladder[i] << " ";
+        }
     }
     cout <<endl;
 }
